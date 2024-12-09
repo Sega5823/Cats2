637ea743f57ff6305b8934f485e944424f2d995d
Sega5823 <91027350+Sega5823@users.noreply.github.com>
2024-12-09 18:56:16 +0300

    update
    update

--- GateWay/build/resources/main/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT into users
-    (username, password, role, email)
-    values
-        ('s', '$2a$10$lRbeV1.qCgCPRtzWUxcZTuKiSWIjEFlidqW5mcMAx/wA0usD4bYa.', 'ADMIN', 'sega9019@gmail.com');
-                -- 12345
